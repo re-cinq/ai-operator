f680c94ddf8aac24f88fc6e4bcc426127446a321
Sebastian <silenteh@gmail.com>
2025-03-26 15:49:48 +0100

    Adjust tests

--- internal/controller/job_controller_test.go
+++ internal/controller/job_controller_test.go
@@ -51,7 +51,9 @@ var _ = Describe("Job Controller", func() {
 						Name:      resourceName,
 						Namespace: "default",
 					},
-					// TODO(user): Specify other spec details if needed.
+					Spec: aiv1.JobSpec{
+						HuggingFaceSecret: "test-secret",
+					},
 				}
 				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
 			}
